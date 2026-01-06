c992e73ea98e5747d6c046ecdff19c144b3150f3
gca <cher_gia@hotmail.com>
2026-01-06 10:45:40 +0100

    Initial commit: exo slide 106

---
+++ .devcontainer/README.md
@@ -0,0 +1,14 @@
+# .devcontainer for C# (.NET)
+
+This devcontainer uses the official .NET SDK 7.0 image and includes the C# extension.
+
+To use:
+
+- Open the workspace in VS Code.
+- Run the Command Palette command: "Dev Containers: Reopen in Container".
+
+You can also use the Dev Containers CLI:
+
+```powershell
+devcontainer up
+```
---
+++ src/App/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Entre un premier nombre:");
+            int n1, n2;
+            //var n1 = int.Parse(Console.ReadLine() ?? "lmao");
+            if(!int.TryParse(Console.ReadLine(), out n1))
+                throw new Exception("Wrong format, retard !");
+            Console.WriteLine($"{n1} entré; entrez un 2eme nombre:");
+            //var n2 = int.Parse(Console.ReadLine() ?? "lmao");
+            if(!int.TryParse(Console.ReadLine(), out n2))
+                throw new Exception("Wrong format, retard !");
+            Console.WriteLine($"Nombres entrés: {n1} + {n2} = {n1 + n2}");            
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"FUCKED SHIT UP => {ex.Message}");
+        }
+
+
+        
+    }
+}
